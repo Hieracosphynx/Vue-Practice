98ecceedc14da445f0ee257305f3d061eb29f43e
Hieracosphynx <cortezmiguel05@gmail.com>
2020-08-26 21:16:25 -0700

    Event handling

--- practice1/js/main.js
+++ practice1/js/main.js
@@ -3,17 +3,19 @@ var app = new Vue({
     data: {
         product: 'Socks',
         description: 'Warm fuzzy socks',
-        image: './img/socks.jpeg',
+        image: './img/socks-gray.jpeg',
         inventory: 0,
         details: ["80% cotton", "Water resistant", "Gender neutral"],
         variants: [
             {
                 variantId: 1234,
-                variantColor: "Gray"
+                variantColor: "Gray",
+                variantImage: './img/socks-gray.jpeg',
             },
             {
                 variantId: 1235,
-                variantColor: "Black"
+                variantColor: "Black",
+                variantImage: './img/socks-black.jpg',
             }
         ],
         sizes: [
@@ -29,8 +31,25 @@ var app = new Vue({
                 sizeId: 1446,
                 sizeNum: 10.5
             }
-        ]
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart: function() {
+            this.cart+=1
+        },
+        updateProduct: function(variantImage){
+            this.image = variantImage
+        },
         
+        removeFromCart: function(){
+            if(this.cart == 0){
+                alert("Empty cart!")
+            }
+            else{
+                this.cart-=1
+            }
+        }
     }
 })
 
